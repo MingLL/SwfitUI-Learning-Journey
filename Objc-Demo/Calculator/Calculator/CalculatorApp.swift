702f432100a05714781a2d211d89e4ce448a95dd
MingLL <minglli@aliyun.com>
2022-12-03 23:50:40 +0800

    Calculator Demo

---
+++ Objc-Demo/Calculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by MingL L on 2022/11/26.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- Objc-Demo/README.md
+++ Objc-Demo/README.md
@@ -1,2 +1,3 @@
 # Objc中国 相关Demo
 基于Objc中国 相关书籍完成的Demo
+- Calculator [SwiftUI 与 Combine 编程 - 王巍 @onevcat](https://objccn.io/products/swift-ui)
